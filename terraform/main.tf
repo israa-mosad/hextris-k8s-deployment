5d28906244cd3a8af4744f833621784d89251a75
Israa Ramadan Mosaad <133568327+israa-mosad@users.noreply.github.com>
2025-09-29 02:49:48 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.27.0"
     }
+    helm = { // Add Helm provider to manage the deployment
+      source  = "hashicorp/helm"
+      version = "2.12.1"
+    }
   }
 }
 
@@ -11,8 +15,33 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
 resource "kubernetes_namespace" "hextris" {
   metadata {
     name = "hextris"
   }
+}
+
+// Deploy the application using the Helm provider, managing its configuration
+resource "helm_release" "hextris_app" {
+  name       = "hextris"
+  // Point to the local Helm chart directory
+  chart      = "../helm-chart" 
+  namespace  = kubernetes_namespace.hextris.metadata[0].name
+  
+  // Override the image details with variables passed from Jenkins
+  set {
+    name  = "image.repository"
+    value = var.docker_image_repo
+  }
+  set {
+    name  = "image.tag"
+    value = var.docker_image_tag
+  }
+ 
 }
